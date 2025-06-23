77ee36be969ac679c9e191c6c3113961fd15fd44
iwa <iwaongit@gmail.com>
2025-06-23 01:18:06 +0100

    feat: add base Todo struct

---
+++ Sources/structs.swift
@@ -0,0 +1,11 @@
+struct Todo {
+    var id: Int
+    var title: String
+    var completed: Bool
+
+    init(id: Int, title: String, completed: Bool = false) {
+        self.id = id
+        self.title = title
+        self.completed = completed
+    }
+}
