71ac80dae49ec8b8e2be1629c82e27077f072d49
iwa <iwaongit@gmail.com>
2025-06-23 01:18:28 +0100

    feat: make tui look better

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,9 +1,25 @@
 import SwiftTUI
 
-struct MyTerminalView: View {
-  var body: some View {
-    Text("Hello, world!")
-  }
+struct ContentView: View {
+    var body: some View {
+        content
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .border(.rounded)
+    }
+
+    private var content: some View {
+        VStack {
+            Text("   To-Do   ")
+            .bold()
+            .foregroundColor(Color.blue)
+
+            Divider().foregroundColor(Color.gray)
+
+            ForEach(0..<10, id: \.self) { i in
+                Text("Item \(i)")
+            }
+        }
+    }
 }
 
-Application(rootView: MyTerminalView()).start()
+Application(rootView: ContentView()).start()
