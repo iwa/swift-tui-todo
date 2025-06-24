87e8e521a285471599a9a953cf90b438108e7498
iwa <iwaongit@gmail.com>
2025-06-24 22:05:23 +0100

    feat: make completion button look better

--- Sources/Content.swift
+++ Sources/Content.swift
@@ -13,13 +13,13 @@ struct ContentView: View {
 
             ForEach(todoList, id: \.id) { todo in
                 HStack {
-                    Text(" [\(todo.completed ? "x" : " ")] \(todo.id) - \(todo.title)")
-                    Spacer()
-                    Button("Complete") {
+                    Button(" [\(todo.completed ? "x" : " ")] ") {
                         if let index = todoList.firstIndex(where: { $0.id == todo.id }) {
                             todoList[index].toggle()
                         }
                     }
+                    Text("\(todo.id) - \(todo.title)")
+                    Spacer()
                     Button("Remove") {
                         if let index = todoList.firstIndex(where: { $0.id == todo.id }) {
                             todoList.remove(at: index)
